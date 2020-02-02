fbfed039315df606160fb71cfc8cba8c6d032632
alfa-ak <alikaharu.ung@gmail.com>
2020-02-02 11:24:08 +0800

    authentication #2

---
+++ resources/js/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer class="main-footer">
+        <div class="container">
+            <div class="pull-right hidden-xs">
+                <b>Version</b> 1.0.0
+            </div>
+            <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Alfa Project</a>.</strong> All rights
+            reserved.
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
