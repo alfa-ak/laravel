ea3d66468587c6f0fa37e4a2fdb0a8d984091358
alfa-ak <alikaharu.ung@gmail.com>
2020-02-19 22:21:28 +0800

    Management Couriers #4

--- resources/js/router.js
+++ resources/js/router.js
@@ -10,6 +10,11 @@ import DataOutlet from './pages/outlets/Outlet.vue'
 import AddOutlet from './pages/outlets/Add.vue'
 import EditOutlet from './pages/outlets/Edit.vue'
 
+import IndexCourier from './pages/couriers/Index.vue'
+import DataCouriers from './pages/couriers/Courier.vue'
+import AddCouriers from './pages/couriers/Add.vue'
+import EditCouriers from './pages/couriers/Edit.vue'
+
 Vue.use(Router)
 
 //DEFINE ROUTE
@@ -32,6 +37,7 @@ const router = new Router({
         {
             path: '/outlets',
             component: IndexOutlet,
+            meta: { requiresAuth: true },
             children: [
                 {
                     path: '',
@@ -51,6 +57,34 @@ const router = new Router({
                     component: EditOutlet,
                     meta: { title: 'Edit Outlet' }
                 }
+            ]
+        },
+
+        //kode courrier
+        {
+            path: '/couriers',
+            component: IndexCourier,
+            meta: { requiresAuth: true },
+            children: [
+                {
+                    path: '',
+                    name: 'couriers.data',
+                    component: DataCouriers,
+                    meta: { title: 'Manage Couriers' }
+                },
+                {
+                    path: 'add',
+                    name: 'couriers.add',
+                    component: AddCouriers,
+                    meta: { title: 'Add New Courier' }
+                },
+                {
+                    path: 'edit/:id',
+                    name: 'couriers.edit',
+                    component: EditCouriers,
+                    meta: { title: 'Edit Courier' }
+                },
+                //NANTINYA AKAN BERISI CHILDREN LAINNYA DARI PAGE COURIERS
             ]
         }
     ]
