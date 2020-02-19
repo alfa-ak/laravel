a3d7f526d74c044da338bc41524aabe1a4d4bc5f
alfa-ak <alikaharu.ung@gmail.com>
2020-02-19 21:42:20 +0800

    Management Outlets #3

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('/login', 'Auth\LoginController@login');
+
+Route::group(['middleware' => 'auth:api'], function() {
+    Route::resource('/outlets', 'API\OutletController')->except(['show']);
+});
